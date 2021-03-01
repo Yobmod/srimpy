a4a768bc6097b804f72d8369497807821fe16000
yobmod <yobmod@gmail.com>
2021-03-01 16:43:38 +0000

    add py.typed

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages  
+from setuptools import setup, find_packages
 from os import path
 # from codecs import open
 from pathlib import Path
@@ -10,34 +10,34 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name='srimpy', 
-    version='0.6.3',  
+    name='srimpy',
+    version='0.6.3',
     description='Wrapper scripts for Srim analysis and multi-processing',
-    long_description=README, 
-    long_description_content_type="text/markdown", 
+    long_description=README,
+    long_description_content_type="text/markdown",
     url='https://gitlab.com/yobmod/srimpy',
-    author='Dominic Laventine', 
+    author='Dominic Laventine',
     author_email='yobmod+srimpy@gmail.com',
-    license="MIT", 
-    classifiers=[ 
+    license="MIT",
+    classifiers=[
         'Development Status :: 4 - Beta',
-        'Natural Language :: English', 
+        'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
-        "Programming Language :: Python :: 3", 
-        'Programming Language :: Python :: 3.6',  
-        'Programming Language :: Python :: 3.7', 
-        'Programming Language :: Python :: 3.8',  
-        'Programming Language :: Python :: 3.9', 
-        'Programming Language :: Python :: 3.10',  
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     keywords='srim analysis ionization parallel processing plotting',
     download_url='https://github.com/Yobmod/srimpy/archive/main.zip',
     packages=find_packages(exclude=['examples', 'tests', 'test_files']),
     package_data={
-        'srim': ['data/*.yaml', 'data/*.toml', 'data/*.json' ],
+        'srim': ['data/*.yaml', 'data/*.toml', 'data/*.json', 'srim/py.typed'],
     },
     include_package_data=True,
-    setup_requires=['pytest-runner', 'setuptools>=40.0.0'],  
+    setup_requires=['pytest-runner', 'setuptools>=40.0.0'],
     install_requires=['pyyaml', 'numpy>=1.15.0' 'matplotlib>=3.0.0'],
     tests_require=['pytest', 'pytest-mock', 'pytest-cov', 'pytest-srcpaths'],
 )
